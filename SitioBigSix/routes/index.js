72704755f9a4fa7b17c27013740b9fbec3a5f4db
Ignacio Cardozo <i_gna77@live.com.ar>
2023-09-23 20:12:02 -0300

    tarea unidad 4 sesiones y middleware(ingreso)

--- SitioBigSix/routes/index.js
+++ SitioBigSix/routes/index.js
@@ -4,7 +4,7 @@ var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Big Six prueba ' });
 });
 
 router.post('/', async (req, res, next) => {
